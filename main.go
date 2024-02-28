375c12ba9f1a2ff76ba3704f600d6bf4e233bed5
David Hisel <david.hisel@cyberark.com>
2024-02-28 03:18:10 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+import (
+"log"
+)
+
+// This is a GG sandbox file ... creds are intentionally going to be put in here
+// while developing an integration with GG
+
+func main() {
+  // $ printf "hello world - 1709089893" | sha256sum 
+  api_key := "b2751aa8eb0cf0cfab978df74358392ba81b00cb4446f96d9d16b3247659999a"
+  log.Println(api_key)
+}
---
+++ simulate-developer-commit-push-secret.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash 
+
+export MY_TIMESTAMP=${EPOCHSECONDS:-$(date +%s)}
+if [ -z "$MY_TIMESTAMP" ]; then
+    echo "Could not create timestamp, exiting."
+    exit 1
+fi
+
+export HASH="$(printf 'hello world - %s' $MY_TIMESTAMP | sha256sum | awk '{print $1}')"
+if [ -z "$HASH" ]; then
+    echo "Empty hash, exiting."
+    exit 2
+fi
+
+read -r -d '' TMPL <<-EOF
+	package main
+	import (
+		"log"
+	)
+	
+	// This is a GG sandbox file ... creds are intentionally going to be put in here
+	// while developing an integration with GG
+	
+	func main() {
+	  // $ printf "hello world - $MY_TIMESTAMP" | sha256sum 
+	  api_key := "$HASH"
+	  log.Println(api_key)
+	}
+EOF
+
+eval "echo '$TMPL' > main.go"
+
+git add main.go
+git commit -m "rev $MY_TIMESTAMP"
+git push
+
